6f2385b0475b08af3d8eb01f947be1755e3346b0
RaminTuzelis <k.mickunas@gmail.com>
2024-08-20 18:46:43 +0300

    Created Connection to Mysql.

--- nbb/server.js
+++ nbb/server.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql2');
 
 const app = express();
 const port = 3001;
 
 // CORS middleware
 app.use(cors());
+
+// Create Mysql Connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'notebook'
+});
+
+// Check connection
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connect to Mysql:', err);
+    return;
+  }
+  console.log('Successfully connected to Mysql database!');
+});
+
 
 // Pagrindinis endpointas
 app.get('/', (req, res) => {
